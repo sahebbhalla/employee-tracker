2b954b546b1eaa48536184ad68222d805723ec48
saheb <sahibbhalla.010@gmail.com>
2022-09-18 17:07:55 -0400

    Added POST and get methods for departments

---
+++ routes/apiRoutes/departmentRoute.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const db =require('../../db/connection');
+
+router.get('/departments',(req,res)=>{
+    const sql ="SELECT * FROM department";
+
+    db.query(sql,(err,results)=>{
+        if(err){
+            res.status(500).json({error:err.message});
+            return;
+        }
+        res.json({
+            message:"Success",
+            data:results
+        })
+    })
+})
+router.post('/departments',({body},res)=>{
+    const sql ="INSERT INTO department(name) VALUES(?)";
+    
+    db.query(sql,body.department_name,(err,results)=>{
+        if(err){
+            res.status(500).json({error:err.message});
+            return;
+        }
+        res.json({
+            message:"Success",
+            data:results.affectedRows
+        })
+    })
+})
+
+module.exports = router;
